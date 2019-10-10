7a5b6648c4296ce9b6a69247c39fdb2f87b25ba2
Michael Williams <manicdesigner@gmail.com>
2019-10-10 15:55:52 -0400

    add files and test files

--- baseball-at-bat/src/App.js
+++ baseball-at-bat/src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import Strikes from './components/dashboard/Strikes';
 import './App.css';
 
 function App() {
+  const [strikes, setStrikes] = useState(0);
   return (
     <div className='App'>
       <h1>Baseball</h1>
