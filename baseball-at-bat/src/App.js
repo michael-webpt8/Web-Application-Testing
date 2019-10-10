1c9ac49c52e08b449136cba7a00acb0860c5daa6
Michael Williams <manicdesigner@gmail.com>
2019-10-10 16:14:40 -0400

    add display component to render to screen

--- baseball-at-bat/src/App.js
+++ baseball-at-bat/src/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import Strikes from './components/dashboard/Strikes';
 import './App.css';
+import Display from './components/display/Display';
 
 function App() {
   const [strikes, setStrikes] = useState(0);
   return (
     <div className='App'>
       <h1>Baseball</h1>
+      <Display />
     </div>
   );
 }
