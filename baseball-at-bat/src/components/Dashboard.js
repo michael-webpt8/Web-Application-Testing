aba98574740a5c4a003d008850491fd45c6b01fe
Michael Williams <manicdesigner@gmail.com>
2019-10-10 19:37:53 -0400

    add buttons to Dashboard

---
+++ baseball-at-bat/src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Dashboard(props) {
+  return (
+    <div>
+      <button>Strike</button>
+      <button>Ball</button>
+      <button>Foul</button>
+      <button>Hit</button>
+    </div>
+  );
+}
+export default Dashboard;
