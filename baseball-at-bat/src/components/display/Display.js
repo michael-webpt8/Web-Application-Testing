fb519bd42dbbaf9b2bd71cef2c9120e4631591eb
Michael Williams <manicdesigner@gmail.com>
2019-10-10 16:14:17 -0400

    mock up display board

---
+++ baseball-at-bat/src/components/display/Display.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Display = props => {
+  return (
+    <>
+      <h3>Display board</h3>
+      <div className='display-board'>
+        <div>
+          <h3>Strike</h3>
+          <p>0</p>
+        </div>
+        <div>
+          <h3>Ball</h3>
+          <p>0</p>
+        </div>
+        <div>
+          <h3>Foul</h3>
+          <p>0</p>
+        </div>
+        <div>
+          <h3>Hit</h3>
+          <p>0</p>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Display;
