ce99c6e9909ca4b8e4588bf6b83a8d40e10ad795
Michael Williams <manicdesigner@gmail.com>
2019-10-10 20:18:53 -0400

    removed foul state

--- baseball-at-bat/src/App.js
+++ baseball-at-bat/src/App.js
@@ -7,7 +7,6 @@ import Dashboard from './components/Dashboard';
 function App() {
   const [strike, setStrike] = useState(0);
   const [ball, setBall] = useState(0);
-  const [foul, setFoul] = useState(0);
   const [hit, setHit] = useState(0);
 
   const strikes = () => {
